78cbe2fd40715ea188354abba3c984999a330e9e
phat_sumo <ethanjpayne1@gmail.com>
2024-04-24 12:33:44 -0600

    update trip to new configuration

--- map_trip.c
+++ map_trip.c
@@ -5,7 +5,7 @@
 
    todo: make some sick ascii art
 
-1920x1080 - 2560x1440 - 1920x1080
+1920x1080 - 2560x1440 - 2560x1440
 
 */
 
@@ -13,11 +13,11 @@ void map_init(Display *dpy) {
 }
 
 void map(int *x, int *y) {
-    int w = 2560 + 1920*2;
+    int w = 2560*2 + 1920;
     WRAP(x, 0, w-2);
     WRAP(x, w-1, 1);
 
-    int h = ((*x < 1920) || (*x > 1920+2560)) ? 1080 : 1440;
+    int h = (*x < 1920) ? 1080 : 1440;
     WRAP(y, 0, h-2);
     WRAP(y, h-1, 1);
 }
